26d6c25542943f423996f408a193f8c56de69574
Platane <me@platane.me>
2016-04-09 17:28:22 +0200

    previus and actual state in fragment diff

--- src/web_modules/component/fragmentDiff/fragmentDiff.jsx
+++ src/web_modules/component/fragmentDiff/fragmentDiff.jsx
@@ -1,13 +1,28 @@
 import React, {PropTypes} from 'react'
+import ObjectTree         from 'component/objectTree/main.jsx'
 
-const FragmentDiff = ({ diff, nodeName }) =>
-(
+const FragmentDiff = ({ diff, nodeName }) => {
+    const {after, before} = diff || {}
 
-    <div className="fragmentDiff">
-        <div className="fragmentDiff-name">{ nodeName }</div>
-    </div>
+    return (
 
-)
+        <div className="fragmentDiff">
+            <div className="fragmentDiff-name">{ nodeName }</div>
+            <div className="fragmentDiff-body">
+
+                <div className="fragmentDiff-afterState">
+                    <ObjectTree object={ after }/>
+                </div>
+
+                <div className="fragmentDiff-beforeState">
+                    <ObjectTree object={ before }/>
+                </div>
+
+            </div>
+        </div>
+
+    )
+}
 
 FragmentDiff.PropTypes = {
     nodeName : PropTypes.string.isRequired,
